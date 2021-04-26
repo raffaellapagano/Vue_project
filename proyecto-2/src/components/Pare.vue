036f5ab1eb1480eec90ae3572638f6af7fd970dc
raffaellapagano <raffaellapagano@yahoo.it>
2021-04-26 17:49:45 +0200

    Comienzo

---
+++ proyecto-2/src/components/Pare.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="pare">
+    <Fill msg="Soc el fill"/>
+  </div>
+</template>
+
+<script>
+import Fill from '../components/Fill.vue'
+
+export default {
+  name: 'Pare',
+   components: {
+    Fill
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
